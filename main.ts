f4d351022a3c73542fa396029632378ea41ae71a
anthonypangallo56-cmd <anthonypangallo56@gmail.com>
2025-08-13 10:54:19 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("CORRECT0")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("INCORRECT0")
+})
+basic.showString("PARA SER MAS PEQUEÑO QUE LOS DEMAS, SE LEVANTA MAS QUE TODOS SI ESTA MOTIVADO Y CONTENTO. ¿QUE ES?")
+basic.showString("(A) EL DEDO PULGAR")
+basic.showString("(B) LA MANO")
